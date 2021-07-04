9731639ef7e9b898271e5d9c8620cff9db1406e6
surajit14cse <surajit14@cse.pstu.ac.bd>
2021-07-04 08:42:33 -0700

    Comparing different values

--- src/app.js
+++ src/app.js
@@ -8,3 +8,15 @@ function testString(val) {
 }
 
 console.log(testString(12));
+
+//Comparing different values
+function compareEqual(a, b) {
+    if (a == b) {
+        return "Equal";
+    }
+
+    return "Not Equal";
+}
+
+console.log(compareEqual(10, "10"));
+console.log(compareEqual(100, "10"));
