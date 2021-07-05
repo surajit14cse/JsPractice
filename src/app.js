8fa8e10d1892a78d2779c1e8c8073d5a92576474
surajit14cse <surajit14@cse.pstu.ac.bd>
2021-07-05 10:37:25 -0700

    Switch Statements

--- src/app.js
+++ src/app.js
@@ -115,3 +115,28 @@ function golfScore(par, strokes) {
     }
 }
 console.log(golfScore(4, 6));
+
+//Switch Statements
+
+function caseInSwitch(val) {
+    var answer = "";
+    switch(val){
+        case 1:
+            answer = "alpha";
+            break;
+    
+        case 2:
+        answer = "beta";
+        break;
+
+        case 3:
+        answer = "gamma";
+        break;
+
+        case 4:
+        answer = "delta";
+        break;
+    }
+    return answer;
+}
+console.log(caseInSwitch(3));
