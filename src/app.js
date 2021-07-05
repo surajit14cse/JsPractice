4b0d17282b097d9ca15c16ed8ad0f83672721afc
surajit14cse <surajit14@cse.pstu.ac.bd>
2021-07-05 10:21:10 -0700

    Usaing Golf Score

--- src/app.js
+++ src/app.js
@@ -88,3 +88,30 @@ function testSize(num) {
     }
 }
 console.log(testSize(21));
+//Usaing Golf Score
+
+var names = ["Hele-in-one", "Eagle", "Birdie", "par", "Bogey", "Double Bogey"]
+function golfScore(par, strokes) {
+    if (strokes == 1) {
+        return names [0]
+    }
+    else if (strokes <= par - 2) {
+        return names [1]
+    }
+    else if (strokes == par -1) {
+        return names [2]
+    }
+    else if (strokes == par) {
+        return names [3]
+    }
+    else if (strokes == par + 1) {
+        return names [4]
+    }
+    else if (strokes == par +2) {
+        return names [5]
+    }
+    else if (strokes >= par +3) {
+        return names [6]
+    }
+}
+console.log(golfScore(4, 6));
