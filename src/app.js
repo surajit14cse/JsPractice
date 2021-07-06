363c2550d99813609db7358a701844473b8f933c
surajit14cse <surajit14@cse.pstu.ac.bd>
2021-07-06 10:36:34 -0700

    Return Early Pattern for Functions

--- src/app.js
+++ src/app.js
@@ -256,3 +256,13 @@ function isLess (a, b) {
     return a < b;
 }
 console.log(isLess(50, 15));
+
+//Return Early Pattern for Functions
+function abTest(a, b) {
+    if (a < 0 || b <0) {
+        return undefined
+    }
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+
+}
+console.log(abTest(7,4));
