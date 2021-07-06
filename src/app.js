8d296b01de203eea8a5f70c8e81ce35c2f812e7f
surajit14cse <surajit14@cse.pstu.ac.bd>
2021-07-06 09:08:33 -0700

    Day In Month

--- src/app.js
+++ src/app.js
@@ -217,3 +217,35 @@ function chainToSwitch(val) {
 }
 console.log(chainToSwitch(99));
 
+//Day In Month
+
+function dayInMonth(month, year) {
+    switch (month) {
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return 30;
+        case 2:
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                return 29;
+
+            }
+            else 
+                return 28;
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            return 31;
+
+        default: 
+
+          return "Invalid";
+    }
+
+}
+console.log(dayInMonth(9, 2020));
